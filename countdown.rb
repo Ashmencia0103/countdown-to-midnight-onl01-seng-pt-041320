b487a673cfd6326862cfbf7f85aa83f2901cd4c2
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-19 19:14:35 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
-
-def countdown
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+     x -= 1
+     sleep 1
+   end 
+end
+
+# def using_while
+#   levitation_force = 6
+# 	while levitation_force < 10
+#     puts "Wingardium Leviosa"
+#     levitation_force += 1
+#   end
+# end
